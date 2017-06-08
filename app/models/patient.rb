7d0ddb8b6fa2673872ccf865546000fbad43f287
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2017-06-08 20:36:54 +0200

    seeded

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ApplicationRecord
   has_many :consultations
-  has_many :doctors, though: :consultations
+  has_many :doctors, through: :consultations
 
   validates :name, presence: true
 end
